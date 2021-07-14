2bfa28a70103a78f1f2d41aefd143e9f6986a2dc
spstolar <simonstolarczyk@gmail.com>
2021-07-13 23:05:06 -0500

    Add upstream crates.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
+use log::info;
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
-use libdaisy::prelude::*;
-
-use log::info;
 // Includes a panic handler and optional logging facilities
 use libdaisy::logger;
 
@@ -20,6 +17,7 @@ use stm32h7xx_hal::timer::Timer;
 use libdaisy::gpio;
 use libdaisy::prelude::*;
 use libdaisy::system;
+
 
 #[rtic::app(
     device = stm32h7xx_hal::stm32,
